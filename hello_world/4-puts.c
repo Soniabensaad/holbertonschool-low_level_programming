ea5ee412cab77eddefbd6f0f988ec4efdb90bee0
Soniabensaad <5687@holbertonstudents.com>
2022-09-29 14:37:07 -0700

    print

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,14 +3,11 @@
 /**
  *  * main - Entry point
  *   *
- *    * Description: 'Just print string'
- *     *
- *      * Return: Always 0 (Success)
- *       */
-
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
-	
+
