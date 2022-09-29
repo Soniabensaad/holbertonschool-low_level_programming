4262a73a50dfdba10797e64b608f11d94dc43195
Soniabensaad <5687@holbertonstudents.com>
2022-09-29 14:50:19 -0700

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <sudio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,");
+return(0);
+}
