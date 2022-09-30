154c5c815917a025112105455148724731c852d7
Soniabensaad <5687@holbertonstudents.com>
2022-09-30 12:46:02 -0700

    unix

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,5 @@
-#include<unistd.h>
+#include <unistd.h>
+#include <stdio.h>
 /**
  *main - Entry point for c program
  *
@@ -6,7 +7,7 @@
  */
 int main(void)
 {
-write(stderr,"and that piece of art is useful" - Dora Korpar, 2015-10-19"));
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
 
