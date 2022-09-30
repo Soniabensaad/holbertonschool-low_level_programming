8c994bb0bd0896d30e76637b9ef50af15057e307
Soniabensaad <5687@holbertonstudents.com>
2022-09-30 01:52:25 -0700

    the size of various types on the computer it is compiled and run on

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,11 +10,11 @@ float floatType;
 long long int inType;
 long int inType;
 char charType;
-printf("Size of char: %zu byte\n", sizeof(charType));
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of long int: %zu bytes\n", sizeof(long intType));
-printf("Size of long long int: %zu bytes\n", sizeof(long long intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
+printf("Size of char: %zu byte(s)\n", sizeof(charType));
+printf("Size of int: %zu byte(s)\n", sizeof(intType));
+printf("Size of long int: %zu byte(s)\n", sizeof(longintType));
+printf("Size of long long int: %zu byte(s)\n", sizeof(longlongintType));
+printf("Size of float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
 
