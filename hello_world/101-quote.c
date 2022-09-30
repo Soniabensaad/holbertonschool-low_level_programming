fa7c12b78c9f1aad97ab21a9fa7417d8e43b4df8
Soniabensaad <5687@holbertonstudents.com>
2022-09-30 06:46:05 -0700

    Intel

---
+++ hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<unistd.h>
+/**
+ *main - Entry point for c program
+ *
+ *Return: Always 1 (Success)
+ */
+int main(void)
+{
+write(stderr,"and that piece of art is useful" - Dora Korpar, 2015-10-19"));
+return (1);
+}
+
