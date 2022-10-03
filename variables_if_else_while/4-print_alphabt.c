da5d4ca0b9005ca4abc92b13d09edd627d8a984a
Soniabensaad <5687@holbertonstudents.com>
2022-10-03 18:28:48 +0200

    alphabet except q ,e

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 97; i <=122; i++)
-         {
-            if (i != 101 && i != 113)
-	       {
-		   putchar(i);
-	       }
-	 }
+	for (i = 97; i <= 122; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
 	putchar('\n');
-	return(0);
+
+	return (0);
 }
