0dde6c41efcfce8b6894564fee45df57ef098788
Soniabensaad <5687@holbertonstudents.com>
2022-10-03 21:22:26 +0200

    possible combinations of single-digit numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - possible combinations of single-digit numbers
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int letter;
+for (letter = '0'; letter <= '9'; letter++)
+{
+putchar(',');
+putchar(' ');
+}
+putchar(n);
+putchar('\n');
+return (0);
+}
