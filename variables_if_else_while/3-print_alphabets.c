1cb0532094b5f72cd14c5a41ac1601be9d980947
Soniabensaad <5687@holbertonstudents.com>
2022-10-03 14:54:31 +0200

    print alphabet

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -11,11 +11,12 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-
-        for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
-	putchar('\n');
 
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
 
 	return (0);
 }
