0d2551acfff6a45d3245ce53e957ba7dd856ca1c
Soniabensaad <5687@holbertonstudents.com>
2022-10-03 20:49:38 +0200

    the lowercase alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -that prints the lowercase alphabet in reverse
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int letter;
+
+	for (letter ='z'; letter <='a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
