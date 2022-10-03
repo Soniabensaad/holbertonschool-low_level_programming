2db993eba9ee4e023525ce7764283123d48057c6
Soniabensaad <5687@holbertonstudents.com>
2022-10-03 18:16:17 +0200

    alphabet except q ,e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet except q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <=122; i++)
+         {
+            if (i ==101 && i ==113)
+	       {
+		       putchar(i);
+	       }
+	 }
+	putchar('\n');
+	return(0);
+}
