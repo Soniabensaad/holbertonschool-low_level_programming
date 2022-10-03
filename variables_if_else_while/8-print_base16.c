bd6672649c34dbc31b8ed797cf1c508d4133ea8c
Soniabensaad <5687@holbertonstudents.com>
2022-10-03 20:59:11 +0200

    numbers of base 16 in lowercase

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int letter;
+        for (letter = '0'; letter <= '9'; letter++)
+	{
+		putchar(letter);
+	}
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
