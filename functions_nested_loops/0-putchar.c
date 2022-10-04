16cf030b2783cccabd89d7a61bd78d76b0464754
Soniabensaad <5687@holbertonstudents.com>
2022-10-04 17:59:08 +0200

    print putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,15 +1,18 @@
-#include <stdio.h>
-
-/**
- * main - possible combinations of single-digit numbers
- *
- * Return: Always 0.
- */
-
+#include "main.h"
 int main(void)
+/**main - prints putchar
+ * Return: Always 0
+ */
 {
-int putchar;
-printf("_putchar\n");
-
-return (0);
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+	
+	return(0);
 }
