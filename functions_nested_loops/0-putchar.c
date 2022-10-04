7a552d73a117ce7213f0ecca3f4da0b4503a0476
Soniabensaad <5687@holbertonstudents.com>
2022-10-04 11:20:31 +0200

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - possible combinations of single-digit numbers
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int putchar;
+printf("_putchar\n");
+
+return (0);
+}
