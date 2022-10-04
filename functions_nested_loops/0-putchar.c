ca44e2009185c9ce520c6af3ef54e5035efb7520
Soniabensaad <5687@holbertonstudents.com>
2022-10-04 18:06:38 +0200

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -4,15 +4,15 @@ int main(void)
  * Return: Always 0
  */
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	
 	return(0);
 }
