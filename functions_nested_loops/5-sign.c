1f2e46da331ade50a1d31bc8c57a32524c70d3f4
Soniabensaad <5687@holbertonstudents.com>
2022-10-05 14:09:46 +0200

    a signe

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,28 +1,25 @@
 #include "main.h"
 
 /**
- * prints the sign of a number - Entry point
- *
- * Description: sign of a number positive or negative
- *
- * Return: Always void (Success)
+ * print_sign - Entry point
+ * @n: int
+ * Return: 0 1 -1
  */
 int print_sign(int n)
 {
-if (n > 0)
-{
-_putchar ('+');
-return (1);
+    if (n > 0)
+    {
+        _putchar('+');
+        return (1);
+    }
+    else if (n == 0)
+    {
+        _putchar('0');
+        return (0);
+    }
+    else
+    {
+        _putchar('-');
+        return (-1);
+    }
 }
-else if (n == 0)
-{
-_putchar ('0');
-return (0);
-}
-else
-{
-_putchar ('-');
-return (-1);
-}
-}
-
