ce84b631210ea9019215a68bdf784c4ec441b001
Soniabensaad <5687@holbertonstudents.com>
2022-10-05 13:52:19 +0200

    isalpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -7,13 +7,13 @@
  *
  * Return: Always 0
  */
+
 int _isalpha(int c)
 {
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    {
+        return (1);
+    }
 
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-{
-    return (1);
-}
- 
-return (0);
+    return (0);
 }
