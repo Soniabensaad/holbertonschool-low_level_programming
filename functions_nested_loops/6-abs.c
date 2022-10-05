1a6f6e6b627d957678ef9d3b75fb653371ab053a
Soniabensaad <5687@holbertonstudents.com>
2022-10-05 14:18:42 +0200

    abs

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * abs of number - Entry point
+ * _abs -a function that computes the absolute value of an integer
  *
- * Description: computes the absolute value of an integer.
+ * @a:int
  *
- * Return: Always integer (Success)
+ * Return: Always 0
  */
 
 int _abs(int i)
