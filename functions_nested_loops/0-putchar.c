d54d30b41c200eb9f05d38fee23cc61476880f2d
Soniabensaad <5687@holbertonstudents.com>
2022-10-05 10:19:33 +0200

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,21 +1,23 @@
 #include"main.h"
 /**
- * print_alphabet_x10 - Entry point
+ * print putchar - Entry point
  *
  * Description: Prints Putchar
  *
- * Return: Always void (Success)
+ * Return: Always void (Success)}
  */
-int _putchar(char c)
+int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
 	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
+
 	return (0);
 }
