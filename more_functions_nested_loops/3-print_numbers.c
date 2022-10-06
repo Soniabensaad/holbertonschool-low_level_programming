bef4f385f18782fbe7b8a7d4b37ceb3f86579cd9
Soniabensaad <5687@holbertonstudents.com>
2022-10-06 21:47:59 +0200

    print numbers

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -8,11 +8,11 @@
  */
 void print_numbers(void)
 {
-    int n;
-    for (n = 48; n <= 57; n++)
-    {
-        _putchar(n);
-    }
-    _putchar('\n');
+int n;
+for (n = 48; n <= 57; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
 }
 
