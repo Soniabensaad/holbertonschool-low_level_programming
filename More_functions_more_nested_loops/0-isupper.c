944160afb2681db4ac45375b7bba9bd157c9d660
Soniabensaad <5687@holbertonstudents.com>
2022-10-06 18:26:10 +0200

    isupper

---
+++ More_functions_more_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isupper -checks for uppercase character
+ *
+ *@c: integer
+ *
+ * Description:  Upercase of character
+ *
+ * Return: Always 0 or 1
+ */
+
+int _isupper(int c)
+{
+
+
+    if (c >= 65 && c <= 90)
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
