6d4f7040e729c515cbbac73a39b2daea607c88a6
Soniabensaad <5687@holbertonstudents.com>
2022-10-06 21:44:11 +0200

    print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _numbers - prints the numbers
+ *
+ * Description: prints the numbers, from 0 to 9, followed by a new line.
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+int n;
+for (n = 48; n <= 57; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
+
