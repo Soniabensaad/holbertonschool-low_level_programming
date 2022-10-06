b154aa832b00037c67e3172f0777fd3849d53dbb
Soniabensaad <5687@holbertonstudents.com>
2022-10-06 22:15:29 +0200

    mostnumbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,15 +8,14 @@
  */
 void print_most_numbers(void)
 {
-int n;
-for (n = 0; n <= 9; n++)
-{
-if (n == 2 || n == 4)
-{
-continue;
-}
-    
-_putchar(n +'0');
-}
-_putchar('\n');
+    int n;
+    for (n = 0; n <= 9; n++)
+    {
+        if (n == 2 || n == 4)
+        {
+            continue;
+        }
+        _putchar(n + '0');
+    }
+    _putchar('\n');
 }
