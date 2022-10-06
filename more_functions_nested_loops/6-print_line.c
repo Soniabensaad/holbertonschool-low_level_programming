ba421c9f3800cfab04dad8cffcf36bf247e44f48
Soniabensaad <5687@holbertonstudents.com>
2022-10-06 23:33:19 +0200

    printline

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -10,13 +10,14 @@
  */
 void print_line(int n)
 {
-    int a;
-    if (n > 0)
-    {
-        for (a = 0; a < n; a++)
-        {
-            _putchar('_');
-        }
-    }
-    _putchar('\n');
+int a;
+if (n > 0)
+{
+for (a = 0; a < n; a++)
+{
+_putchar('_');
 }
+}
+_putchar('\n');
+}
+
