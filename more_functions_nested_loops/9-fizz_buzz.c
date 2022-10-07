1d7da700d2189dae7bc39cb42d70c6e008e4f50b
Soniabensaad <5687@holbertonstudents.com>
2022-10-07 12:30:29 +0200

    fizz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,31 +11,31 @@
  */
 int main(void)
 {
-    int n;
+int n;
 
-    for (n = 1; n < 101; n++)
-    {
-        if (n % 3 == 0 && n % 5 == 0)
-        {
-            printf("FizzBuzz");
-        }
-        else if (n % 3 == 0)
-        {
-            printf("Fizz");
-        }
-        else if (n % 5 == 0)
-        {
-            printf("Buzz");
-        }
-        else
-        {
-            printf("%d", n);
-        }
-        if (n != 100)
-        {
-            putchar(' ');
-        }
-    }
-    putchar('\n');
-    return (0);
+for (n = 1; n < 101; n++)
+{
+if (n % 3 == 0 && n % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (n % 3 == 0)
+{
+printf("Fizz");
+}
+else if (n % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", n);
+}
+if (n != 100)
+{
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
