84be94345962ba11d5225ac51cadb7929ea62d14
Soniabensaad <5687@holbertonstudents.com>
2022-10-09 00:49:50 +0200

    prime

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * prime_factor  - prints the largest prime factor number
+ *
+ * @n:integer
+ *
+ * Description:prints the largest prime factor number
+ *
+ * Return 0
+ */
+
+int main() 
+{
+long int i, a, b;
+a = 612852475143;
+
+for (i = 1; i <= a; i++)
+{
+if (a % i == 0)
+{
+if (a == i)
+{
+printf("%ld\n", i);
+break;
+}
+b = a / i;
+a = b;
+i = 1;
+}
+}
+return (0);
+}
