b5f316d4887f92718f59cbe65b791e2e50e0db18
Soniabensaad <5687@holbertonstudents.com>
2022-10-09 00:52:22 +0200

    prime

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -12,22 +12,23 @@
 
 int main()
 {
-    long int i, a, b;
-    a = 612852475143;
+long int i, a, b;
+a = 612852475143;
 
-    for (i = 1; i <= a; i++)
-    {
-        if (a % i == 0)
-        {
-            if (a == i)
-            {
-                printf("%ld\n", i);
-                break;
-            }
-            b = a / i;
-            a = b;
-            i = 1;
-        }
-    }
-    return (0);
+for (i = 1; i <= a; i++)
+{
+if (a % i == 0)
+{
+if (a == i)
+{
+printf("%ld\n", i);
+break;
 }
+b = a / i;
+a = b;
+i = 1;
+}
+}
+return (0);
+}
+
