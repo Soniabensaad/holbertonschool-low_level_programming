df328a33386b4cbc2e9e22ada85a3524346c97ff
Soniabensaad <5687@holbertonstudents.com>
2022-10-09 00:03:01 +0200

    triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,24 +10,24 @@
  */
 void print_triangle(int size)
 {
-    int a, b, c;
-    if (size > 0)
-    {
-        for (a = 0; a < size; a++)
-        {
-            for (b = 1; b <= (size - a ); b++)
-            {
-                _putchar(' ');
-            }
-            for (c = 1; c <= a; c++)
-            {
-                _putchar('#');
-            }
-            _putchar('\n');
-        }
-    }
-    else
-    {
-    _putchar('\n');
-    }
-    }
+int a, b, c;
+if (size > 0)
+{
+for (a = 0; a < size; a++)
+{
+for (b = 1; b <= (size - a ); b++)
+{
+_putchar(' ');
+}
+for (c = 1; c <= a; c++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
