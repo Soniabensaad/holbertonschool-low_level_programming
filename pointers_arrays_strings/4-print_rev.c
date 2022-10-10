7e59d02a7bedc43b5a44b03ffd655c41687265eb
Soniabensaad <5687@holbertonstudents.com>
2022-10-10 21:32:33 +0200

    reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,16 @@
  *
  * Description:  function that prints a string, in reverse
  *
- * Return: 0
+ * Return: void
  */
 void print_rev(char *s)
 {
-int i ;
-while ( s[i] != ('\0'))
+int i = 0;
+while (s[i] != ('\0'))
 {
     i++;
-}
-for (i = i - 1; i >= 0; i--)
+}    
+for (i -= 1; i >= 0; i--)
 {
     _putchar(s[i]);
 }
