bad60340a30506e8089e59d318b0609789cabf65
Soniabensaad <5687@holbertonstudents.com>
2022-10-10 19:22:38 +0200

    swap

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 -takes a pointer to an int
  *
- * @n:ponter of integer
+ * @n:pointer of integer
  *
  * Description:takes a pointer to an int as parameter
  *
---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int -function that swaps the values of two integers
+ *
+ * @a:pointer of integer
+ * @b:pointer of integer
+ *
+ * Description:swaps the values of two integers
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+    int t = *b;
+    *b = *a;
+    *a = t;
+
+}
