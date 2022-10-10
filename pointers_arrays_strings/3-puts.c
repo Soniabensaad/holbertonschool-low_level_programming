4236ac666e5af665d84e1ff0a2ca09e8ee212fdf
Soniabensaad <5687@holbertonstudents.com>
2022-10-10 20:49:06 +0200

    puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen -prints a string, followed by a new line.
+ *
+ * @s:char
+ *
+ * Description: function that prints a string, followed by a new line, to stdout
+ *
+ * Return: 0
+ */
+void _puts(char *str)
+{
+    int i;
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        _putchar(str[i]);
+    }
+    _putchar('\n');
+}
