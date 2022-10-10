71ecbda1ce3edb3307084be2b387f4d4f3d60378
Soniabensaad <5687@holbertonstudents.com>
2022-10-10 23:08:38 +0200

    rev

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,28 +10,18 @@
  */
 void rev_string(char *s)
 {
-int i ;
-int k;
-int c;
-char *a, temp;
-a = s;
+	int i, length, temporaire;
 
-while (s[c] != ('\0'))
-{
-c++;
-}
-for (k = 1; k < c; k++)
-{
-a++;
-}
-for(i = 0; i < (c / 2); i++)
-{
-    temp = s[i];
-    s[i] = *a;
-    *a = temp;
-    a++;
-}
+	length = 0;
+	while (s[length] != '\0')
+	{
+		length ++;
+	}
 
-
+	for (i = 0; i < length / 2; i++)
+	{
+		temporaire = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temporaire;
+	}
 }
-
