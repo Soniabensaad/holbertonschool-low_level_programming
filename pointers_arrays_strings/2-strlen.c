3e2a784292abb32235e202019537900ebd7421f4
Soniabensaad <5687@holbertonstudents.com>
2022-10-10 20:22:05 +0200

    length

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -12,10 +12,10 @@
 int _strlen(char *s)
 {
 int length = 0;
-    while (*s != '\0') {
-        length++;
-        s++;
-    }
- 
-    return length;
+while (*s != '\0') {
+length++;
+s++;
 }
+return length;
+}
+
