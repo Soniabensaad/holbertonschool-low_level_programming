7eaac609b39d8d6d7929349c56d600f0fc786e25
Soniabensaad <5687@holbertonstudents.com>
2022-10-11 22:40:06 +0200

    copy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy -copies the string pointed
+ *
+ * @str:char
+ * @a: char
+ *
+ * Description:  a function that copies the string pointed
+ *
+ * Return: 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+    int index = 0;
+    do{
+    
+        *(dest + index) = *(src + index);
+    } while (*(src + index++));
+    return (dest);
+}
