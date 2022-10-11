1ef5288182a150d3915551826fe02363d37a1690
Soniabensaad <5687@holbertonstudents.com>
2022-10-12 00:03:43 +0200

    concat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy -concatenates two strings
+ *
+ * @dest:char
+ * @src: char
+ *
+ * Description:  a function that  concatenates two strings
+ *
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+i = 0;
+while (dest[i] != '\0')
+{
+    i++;
+}
+for (j = 0; src[j] != '\0'; j++)
+{
+    dest[i+j] = src[j];
+}
+dest[i + j] = '\0';
+return(dest);
+}
+
