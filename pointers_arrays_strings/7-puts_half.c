ceb8c6ee4af4564f28e834ec471a17e4437540fe
Soniabensaad <5687@holbertonstudents.com>
2022-10-11 12:39:35 +0200

    puthalf

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -11,27 +11,27 @@
  */
 void puts_half(char *str)
 {
-    int i, length;
-    length = 0;
-    while (str[length] != '\0')
-    {
-    length++;
-    }
-    if (length % 2 == 0)
-    {
-        for(i = length / 2; i < length ; i++)
-        {
-            _putchar(str[i]);
-        }
-    }
-    else
-    {
-        for(i = (length / 2) + 1; i < length; i++)
-        {
-            _putchar(str[i]);
-        }
-    }
-    _putchar('\n');
+int i, length;
+length = 0;
+while (str[length] != '\0')
+{
+length++;
+}
+if (length % 2 == 0)
+{
+for(i = length / 2; i < length ; i++)
+{
+_putchar(str[i]);
+}
+}
+else
+{
+for(i = (length / 2) + 1; i < length; i++)
+{
+_putchar(str[i]);
+}
+}
+_putchar('\n');
 }
     
    
