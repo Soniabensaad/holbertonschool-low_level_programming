20f69b4a7d1e2548104abed43349b16db6a06955
Soniabensaad <5687@holbertonstudents.com>
2022-10-12 19:04:56 +0200

    reverse

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -16,10 +16,10 @@ int temp;
 n = n - 1;
 while (i < n)
 {
-    temp = *(a + i);
-    *(a + i) = *(a + n);
-    *(a + n) = temp;
-    i++;
-    n--;
+temp = *(a + i);
+*(a + i) = *(a + n);
+*(a + n) = temp;
+i++;
+n--;
 }
 }
