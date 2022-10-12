e4920a6ef5f8f8b8be353e576e1bf5c017dfd7fc
Soniabensaad <5687@holbertonstudents.com>
2022-10-12 22:31:08 +0200

    capitilize

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * string_touppe-changes all lowercase letters of a string to uppercase
+ * string_touppe -changes all lowercase letters of a string to uppercase
  *
  * @s:char
  *
