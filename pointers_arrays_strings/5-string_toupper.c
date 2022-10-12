8c2c4fbb2ab30de675474c1ae83954fab52199f8
Soniabensaad <5687@holbertonstudents.com>
2022-10-12 21:07:31 +0200

    toupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *string_touppe-changes all lowercase letters of a string to uppercase
+ *
+ * @s:char
+ * @n:integer
+ *
+ * Description:a function that changes all lowercase of a string to uppercase.
+ *
+ * Return: void
+ */
+char *string_toupper(char *s)
+{
+	int i, index;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		index = *(s + i);
+		if (index >= 97 && index <= 122)
+			*(s + i) = index - 32;
+	}
+
+	return (s);
+}
