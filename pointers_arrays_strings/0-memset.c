0c49aa2311567dcdb83cf41c0b8869945bed2623
Soniabensaad <5687@holbertonstudents.com>
2022-10-13 11:55:42 +0200

    fills byte

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,25 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _memset- fills the first n bytes of the memory area pointed to by s
+ * _memset- fills the first n bytes of the memory
  *
  * @s: char
  * @n:number of spaces to fill
  * @d:byte to fill with
  *
- * Description: function fills the first n bytes of the memory area pointed to by s
+ * Description: function fills the first n bytes of the memory 
  *
  * Return: 0
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-    
-    
-
-	for (i = 0; i < n; i++)
-	{
-		*(s + i) = b;
-	}
-	return (s);
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+*(s + i) = b;
 }
+return (s);
+}
+
