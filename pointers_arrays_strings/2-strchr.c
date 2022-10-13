1bdb1ce7258b596cdccc2a854b3b145278469526
Soniabensaad <5687@holbertonstudents.com>
2022-10-13 14:17:11 +0200

    strch

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -14,14 +14,13 @@
 char *_strchr(char *s, char c)
 {
  int i;
-
-	for (i = 0; *(s + i); i++)
-	{
-		if (*(s + i) == c)
-			return (s + i);
-	}
-	
+for (i = 0; *(s + i); i++)
+{
 if (*(s + i) == c)
-		return (s + i);
-	return (0);
+return (s + i);
 }
+if (*(s + i) == c)
+return (s + i);
+return (0);
+}
+
