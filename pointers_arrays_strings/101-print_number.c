a58ee70d5c92ddd088d5cbaac7b85ba34c82d253
Soniabensaad <5687@holbertonstudents.com>
2022-10-14 17:01:39 +0200

    printnumbers

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 /**
  * print_number - function with one argument
- * 
- * @n: int type argument
+ *
+ * @b: int type argument
  *
  * Description: prints an integer
- * 
+ *
  * Return: value of n
 */
 void print_number(int b)
@@ -16,7 +16,7 @@ void print_number(int b)
 		_putchar('-');
 		if (b < -9)
 			print_number(b / -10);
-		_putchar ('0' - b % 10 );
+	_putchar ('0' - b % 10 );
 	}
 	else
 	{
