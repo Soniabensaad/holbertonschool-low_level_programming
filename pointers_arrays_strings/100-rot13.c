bd97c98a1db20d32bca5953f5ddd96ced39b6eeb
Soniabensaad <5687@holbertonstudents.com>
2022-10-14 16:16:02 +0200

    rot13

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - encodes a string using rot13.
+ *
+ * @rot13: char
+ * @alphabet:char
+ * @s:char
+ *
+ * Description: a function that encodes a string using rot13.
+ * Return: 0
+*/
+char *rot13(char *str)
+{
+	int i, j;
+
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (alphabet[j] != '\0')
+		{
+			if (str[i] == alphabet[j])
+			{
+				str[i] = rot13[j];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (str);
+}
