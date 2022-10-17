505a8ba05d1f151333c16245fa982b75eface4d1
Soniabensaad <5687@holbertonstudents.com>
2022-10-17 14:56:49 +0200

    strlen

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion-  returns the length of a string
+ *
+ *@s: char
+ *
+ *Description:a function that  returns the length of a string.
+ */
+int _strlen_recursion(char *s)
+{
+    if (*s != '\0')
+    {
+        return (1 + _strlen_recursion(s + 1));
+
+    }
+    else
+    {
+        return (0);
+    }
+    
+}
+
