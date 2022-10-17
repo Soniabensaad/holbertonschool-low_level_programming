72e935fe5d403dc11cdddb61147e5e7acd29c911
Soniabensaad <5687@holbertonstudents.com>
2022-10-17 14:34:50 +0200

    rev

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -9,11 +9,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-    if(*s != '\0')
-    {
-        _print_rev_recursion(s + 1);
-        _putchar(*s);
-        
-    }
+if(*s != '\0')
+{
+ _print_rev_recursion(s + 1);
+_putchar(*s);
+}
 
 }
