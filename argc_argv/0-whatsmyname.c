f0b24571a65f65ca536c2c720b8be5faaa30e425
Soniabensaad <5687@holbertonstudents.com>
2022-10-19 12:20:05 +0200

    myname

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints name, followed by a new line
+ *
+ * @argc : integer
+ * @argv : array of strings
+ *
+ * Description: a program that prints name, followed by a new line.
+ *
+ * Return: Always 0 
+ */
+int main(int argc, char *argv[])
+{
+    argc = 0;
+    printf ("%s\n" , argv[argc]);
+    return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,2 @@
+argc, argv
+in c programming
