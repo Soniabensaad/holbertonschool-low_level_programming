bf52d9b9c91101b930a86c7917f18eb96447c8e9
Soniabensaad <5687@holbertonstudents.com>
2022-10-19 15:24:36 +0200

    prints all arguments that receive

---
+++ argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - prints all arguments it receives.
+ *
+ * @argc : integer
+ * @argv : array of strings
+ *
+ * Description: a program that prints all arguments it receives.
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+    {
+    int i;
+    for(i=0; i<argc ;i++)
+    {
+        printf("%s\n",argv[i]);
+    }
+    return 0;
+}
+}
