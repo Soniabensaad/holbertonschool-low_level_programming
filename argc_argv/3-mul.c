4e3b396ef5c69822f827015cbd32c3717bf739c3
Soniabensaad <5687@holbertonstudents.com>
2022-10-19 20:58:52 +0200

    mul

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -14,22 +14,20 @@
  */
 int main(int argc, char *argv[])
 {int i, mul = 1;
-    if (argc != 3)
-    {
-        
-        printf("%s\n", "error");
-        return (1);
-    }
-    else
-    {
-        for (i = 1; i < argc; i++)
-        {
-            mul *= atoi (argv[i]);
-        }
-        printf("%d\n", mul);
-        
-    }
-    return (0);
-
-
+if (argc != 3)
+{     
+printf("%s\n", "error");
+return (1);
 }
+else
+{
+for (i = 1; i < argc; i++)
+{
+mul *= atoi (argv[i]);
+}
+printf("%d\n", mul);
+        
+}
+    return (0);
+}
+
