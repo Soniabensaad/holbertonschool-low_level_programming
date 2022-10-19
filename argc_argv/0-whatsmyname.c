9ec272575919a7b8b1ce0ecd686d91cf6d664af8
Soniabensaad <5687@holbertonstudents.com>
2022-10-19 12:21:41 +0200

    myname

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -8,11 +8,11 @@
  *
  * Description: a program that prints name, followed by a new line.
  *
- * Return: Always 0 
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-    argc = 0;
-    printf ("%s\n" , argv[argc]);
-    return (0);
+argc = 0;
+printf ("%s\n" , argv[argc]);
+return (0);
 }
