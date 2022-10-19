399f489a83a4e08c5b412c54c0bed6590ba2fb7c
Soniabensaad <5687@holbertonstudents.com>
2022-10-19 14:40:29 +0200

    argument

---
+++ argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints name, followed by a new line
+ *
+ * @argc : integer
+ * @argv : array of strings
+ *
+ * Description: a program that prints name, followed by a new line.
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		argv = argv;
+		printf("%d\n", argc - 1);
+	}
+	return (0);
+}
