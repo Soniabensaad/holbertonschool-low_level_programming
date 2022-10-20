258f778578eeececb42054e6b648ecf382044480
Soniabensaad <5687@holbertonstudents.com>
2022-10-20 21:28:09 +0200

    change

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -27,27 +27,27 @@ if (p < 0)
 printf("0\n");
 return (0);
 }
-if(p >= 25)
+if (p >= 25)
 {
 m = m + p / 25;
 p = p % 25;
 }
-if(p >= 10)
+if (p >= 10)
 {
 m = m + p / 10;
 p = p % 10;
 }
-if(p >= 5)
+if (p >= 5)
 {
 m = m + p / 5;
 p = p % 5;
 }
-if(p >= 2)
+if (p >= 2)
 {
 m = m + p / 2;
 p = p % 2;
 }
-if(p >= 1)
+if (p >= 1)
 {
 m = m + p / 1;
 p = p % 1;
