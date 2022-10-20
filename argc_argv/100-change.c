446efc93c9685f722479c29572344d28d6d38dd4
Soniabensaad <5687@holbertonstudents.com>
2022-10-20 21:27:16 +0200

    change

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -18,7 +18,7 @@ int p, m;
 m = 0;
 if (argc != 2)
 {
-printf ("Error\n");
+printf("Error\n");
 return (1);
 }
 p = atoi(argv[1]);
@@ -27,32 +27,32 @@ if (p < 0)
 printf("0\n");
 return (0);
 }
-if (p >= 25 )
+if(p >= 25)
 {
 m = m + p / 25;
 p = p % 25;
 }
-if (p >= 10 )
+if(p >= 10)
 {
 m = m + p / 10;
 p = p % 10;
 }
-if (p >= 5 )
+if(p >= 5)
 {
 m = m + p / 5;
 p = p % 5;
 }
-if (p >= 2 )
+if(p >= 2)
 {
 m = m + p / 2;
 p = p % 2;
 }
-if (p >= 1 )
+if(p >= 1)
 {
 m = m + p / 1;
 p = p % 1;
 }
-printf ("%d\n", m + p);
+printf("%d\n", m + p);
 return (0);
 }
 
