1dcff1640b99ac2d8615fbdb847ce6416ae2e9e9
Soniabensaad <5687@holbertonstudents.com>
2022-10-21 17:27:13 +0200

    create

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,21 +12,20 @@
  */
 char *create_array(unsigned int size, char c)
 {
-    unsigned int i;
-    char *A;
-    if (size == 0)
-    {
-        return (NULL);
-
-    }
-    A = malloc(size * sizeof(char));
-    if (A == NULL)
-    {
-        return (NULL);
-    }
-    for (i = 0; i < size; i++)
-    {
-        A[i] = c;
-    }
-    return (A);
+unsigned int i;
+char *A;
+if (size == 0)
+{
+return (NULL);
+}
+A = malloc(size * sizeof(char));
+if (A == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+A[i] = c;
+}
+return (A);
 }
