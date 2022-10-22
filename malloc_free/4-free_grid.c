0f86e1c07ecde1d5e5403098846edc7907ef4ae8
Soniabensaad <5687@holbertonstudents.com>
2022-10-22 17:03:06 +0200

    freegrid

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *alloc_grid - nested loop to make grid
+ *
+ *@width: width input
+ * @height: height input
+ *
+ * Description: nested loop to make grid
+ *
+ * Return: pointer to 2
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
