17d98ba4df15f309476c1fcc39de36934e9e0913
Soniabensaad <5687@holbertonstudents.com>
2022-10-22 20:35:48 +0200

    freegrid

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -13,9 +13,9 @@
 
 void free_grid(int **grid, int height)
 {
-	if (grid != NULL || height != 0)
+	if (grid != NULL && height != 0)
 	{
-		while (height > 0)
+		while (height >= 0)
 			free(grid[height--]);
 		free(grid);
 	}
