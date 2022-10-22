d5544f42a6f3864aec9d9563cbda48f9592f4b7b
Soniabensaad <5687@holbertonstudents.com>
2022-10-22 16:15:54 +0200

    grid

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -12,32 +12,33 @@
  */
 int **alloc_grid(int width, int height)
 {
-    char **a;
-    int i, j;
-    
-    if (width <= 0 || height <= 0)
-    {
-        return (NULL);
-    }
-    a = malloc(height * sizeof(int));
-    if (a == NULL)
-    {
-        free(a);
-        return (NULL);
-    }
-    for (i = 0; i < height; i++)
-    {
-        a[i] = malloc(width * sizeof(int))
-        if (a == NULL)
-        {
-            free(a);
-            return (NULL);
-        }
-        for (j = 0; j < width; j++ )
-        {
-            a[i][j] = 0;
-        }
-    }
-    return (a);
+	int **a;
+	int i, j;
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	a = malloc(sizeof(int) * height);
+
+	if (a == NULL)
+	{
+		free(a);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		a[i] = malloc(sizeof(int) * width);
+		if (a == NULL)
+		{
+			free(a);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+		{
+			a[i][j] = 0;
+		}
+	}
+	return (a);
 }
