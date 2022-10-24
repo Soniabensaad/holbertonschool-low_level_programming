fa1358d0d9b7753ee1cf6b75cb676d1216f6d2f4
Soniabensaad <5687@holbertonstudents.com>
2022-10-24 21:12:11 +0200

    arfstostr

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program.
+ * @len: integer
+ * @s: char.
+ *
+ */
+char *argstostr(int ac, char **av)
+{
+    char *s;
+    int i, j, m = 0, len = 0;
+    if (ac == 0 || av == 0)
+    {
+        return (NULL);
+    }
+    for (i = 0; i < ac; i++)
+    {
+        for (j = 0; av[i][j]; j++)
+        {
+            len++;
+        }
+    }
+    len += ac;
+    s = malloc(sizeof(char) * len + 1);
+    if (s == NULL)
+    {
+        return (NULL);
+    }
+    for (i = 0; i < ac; i++)
+    {
+        for (j = 0; av[i][j]; j++)
+        {
+           s[m] = av[i][j];
+           m++;
+        }
+        if (s[m] == '\0')
+        {
+            s[m++] = '\n';
+        }
+    }
+    return (s);
+}
