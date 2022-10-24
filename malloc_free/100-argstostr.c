4b3a79e8b63fff4ac84fa1f448cfd0f46fe3a4cf
Soniabensaad <5687@holbertonstudents.com>
2022-10-24 21:14:08 +0200

    arfstostr

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -10,36 +10,36 @@
  */
 char *argstostr(int ac, char **av)
 {
-    char *s;
-    int i, j, m = 0, len = 0;
-    if (ac == 0 || av == 0)
-    {
-        return (NULL);
-    }
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; j++)
-        {
-            len++;
-        }
-    }
-    len += ac;
-    s = malloc(sizeof(char) * len + 1);
-    if (s == NULL)
-    {
-        return (NULL);
-    }
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; j++)
-        {
-           s[m] = av[i][j];
-           m++;
-        }
-        if (s[m] == '\0')
-        {
-            s[m++] = '\n';
-        }
-    }
-    return (s);
+char *s;
+int i, j, m = 0, len = 0;
+if (ac == 0 || av == 0)
+{
+return (NULL);
+}
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+len++;
+}
+}
+len += ac;
+s = malloc(sizeof(char) * len + 1);
+if (s == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+s[m] = av[i][j];
+m++;
+}
+if (s[m] == '\0')
+{
+s[m++] = '\n';
+}
+}
+return (s);
 }
