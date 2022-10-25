1fa718c5b618c1d31af693b7499f5b8c10f25803
Soniabensaad <5687@holbertonstudents.com>
2022-10-25 19:29:57 +0200

    calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocates memory
+ * @nmemb: unsigned int
+ * @size: unsigned int
+ * Return: Nothing.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+    char *p;
+    unsigned int j;
+    if (nmemb == 0 || size == 0)
+    {
+        return (NULL);
+
+    }
+    p = malloc(nmemb * size);
+    if (p == NULL)
+    {
+
+        return (NULL);
+    }
+    for (j = 0; j < (nmemb * size); j++)
+    {
+        *(p+j) = '\0';
+    }
+    return (p);
+
+}
