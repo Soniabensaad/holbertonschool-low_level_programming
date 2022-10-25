fc20ab3882a09b34824e6c19e11af883bbd3eb08
Soniabensaad <5687@holbertonstudents.com>
2022-10-25 12:36:24 +0200

    mallocchecked

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,19 +3,19 @@
 #include <stdlib.h>
 
 /**
- * argstostr - concatenates all the arguments of your program.
- * @ac: int input
- * @av: double pointer array.
+ * malloc_checked- allocates memory using malloc
+ * @p: pointer 
+ * 
  *Return : 0
  */
 
 void *malloc_checked(unsigned int s)
 {
-    void* p;
-    p = malloc(s);
-    if (p == NULL)
-    {
-        exit(98);
-    }
-    return (p);
+void* p;
+p = malloc(s);
+if (p == NULL)
+{
+exit(98);
+}
+return (p);
 }
