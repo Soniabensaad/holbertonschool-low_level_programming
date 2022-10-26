588215c327ff7fd71583ee71c5a116a4a9f7c616
Soniabensaad <5687@holbertonstudents.com>
2022-10-26 21:51:13 +0200

    pi

--- preprocessor/0-object_like_macro.h
+++ preprocessor/0-object_like_macro.h
@@ -3,6 +3,7 @@
 /**
  * File:0-object_like_macro.h
  * Description: a header file that defines a macro
+    named SIZE as an abbreviation for the token 1024
  */
 #define SIZE 1024
 #endif /*OBJECT_LIKE_MACRO_H*/
