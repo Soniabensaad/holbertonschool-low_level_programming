f7af0c7171723434081174ea1447387a70112c2c
Soniabensaad <5687@holbertonstudents.com>
2022-10-26 21:41:39 +0200

    objectlikemacro

---
+++ preprocessor/0-object_like_macro.h
@@ -0,0 +1,8 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+/**
+ * File:0-object_like_macro.h
+ * Description: a header file that defines a macro
+ */
+#define SIZE 1024
+#endif /*OBJECT_LIKE_MACRO_H*/
