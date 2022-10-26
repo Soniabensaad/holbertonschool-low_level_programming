ed54db0668da3a7b33f9f4392a636fc10b464ca0
Soniabensaad <5687@holbertonstudents.com>
2022-10-26 22:13:49 +0200

    nameof file

---
+++ preprocessor/02-main.c
@@ -0,0 +1,15 @@
+
+/**
+* File: 2-main.c
+*/
+#include <stdio.h>
+/**
+* main- prints the name of the file it was compiled
+*       from, followed by a new line.
+* return :Always 0
+*/
+int main (void)
+{
+    printf ("%s\n", __FILE__);
+    return (0);
+}
