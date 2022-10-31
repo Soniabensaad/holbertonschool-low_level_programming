7aa5dde7f821ed3ef0307d32488adc49c421f9ba
Soniabensaad <5687@holbertonstudents.com>
2022-10-31 13:24:54 +0100

    dogtypedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,9 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog-struct dog
+ * @name : name
+ * @age: age
+ * @owner: owner
+ * Description: struct for a dog 
+*/
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 };
 #endif/* DOG_H*/
