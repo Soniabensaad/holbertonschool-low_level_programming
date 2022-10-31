789975ba7064cad7a2504ee4935c278c9dd6b85f
Soniabensaad <5687@holbertonstudents.com>
2022-10-31 21:13:22 +0100

    structures typedef

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,65 +4,67 @@
 
 
 /**
- * print_dog - Initializes a dog structure
- * @d: A dog structure.
+ * scopy - second function
  *
- * Return: Nothing
+ * @str: string
+ *
+ * Description: 'set string'
+ *
+ * Return: string
  */
 char *scopy (char *t)
 {
-    int i = 0, j = 0;
-    char *m;
-    if (t == NULL)
-    {
-        return (NULL);
-    }
-    
-    while (t[i] != '\0')
-    {
-        i++;
-    }
-    m = malloc(sizeof(*t));
-    if (m == NULL)
-    {
-        return(NULL);
-    }
-    while (t[j] != '\0')
-    {
-        m[j] = t[j];
-        j++;
-    }
-    m[j] = '\0';
-    return (m);
+int i = 0, j = 0;
+char *m;
+if (t == NULL)
+{
+return (NULL);
+}   
+ while (t[i] != '\0')
+{
+i++;
+}
+m = malloc(sizeof(*t));
+if (m == NULL)
+{
+return(NULL);
+}
+while (t[j] != '\0')
+{
+m[j] = t[j];
+j++;
+}
+m[j] = '\0';
+return (m);
 }
 /**
- * print_dog - Initializes a dog structure
- * @d: A dog structure.
+ * new_dog - prints a new dog
+ * @name: A string.
+ * @age:float
+ * @owner:string
  *
- * Return: Nothing
+ * Return: dog_t
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *dog;
-    dog = malloc(sizeof(dog_t));
-    if (dog == NULL)
-    {
-        return (NULL);
-    }
-    dog->age = age;
-    dog->name = scopy(name);
-    if (!dog->name)
-    {
-        free(dog->name);
-        return (NULL);
-    }
-
-    dog->owner = scopy(owner);
-    if (!dog->owner)
-    {
-        free(dog->owner);
-        
-        return (NULL);
-    }
-    return (dog);
+dog_t *dog;
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+{
+return (NULL);
+}
+dog->age = age;
+dog->name = scopy(name);
+if (!dog->name)
+{
+free(dog->name);
+return (NULL);
+}
+dog->owner = scopy(owner);
+if (!dog->owner)
+{
+free(dog->owner);
+return (NULL);
+}
+return (dog);
 }
