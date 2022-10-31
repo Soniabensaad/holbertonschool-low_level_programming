c12ddde7de3ea04a2c29e5f804115b7ec88cfa48
Soniabensaad <5687@holbertonstudents.com>
2022-10-31 16:26:29 +0100

    initdog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
 /**
  * struct dog-struct dog
  * @name : name
@@ -13,4 +16,9 @@ char *name;
 float age;
 char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+
+
 #endif/* DOG_H*/
