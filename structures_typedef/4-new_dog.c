171f8f5e01fe3e5312c5ac208bc375cbb1fd9201
Soniabensaad <5687@holbertonstudents.com>
2022-10-31 20:47:34 +0100

    structures typedef

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,68 @@
+#include "dog.h"
+#include <stdlib.h>
+
+
+
+/**
+ * print_dog - Initializes a dog structure
+ * @d: A dog structure.
+ *
+ * Return: Nothing
+ */
+char *scopy (char *t)
+{
+    int i = 0, j = 0;
+    char *m;
+    if (t == NULL)
+    {
+        return (NULL);
+    }
+    
+    while (t[i] != '\0')
+    {
+        i++;
+    }
+    m = malloc(sizeof(*t));
+    if (m == NULL)
+    {
+        return(NULL);
+    }
+    while (t[j] != '\0')
+    {
+        m[j] = t[j];
+        j++;
+    }
+    m[j] = '\0';
+    return (m);
+}
+/**
+ * print_dog - Initializes a dog structure
+ * @d: A dog structure.
+ *
+ * Return: Nothing
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+    dog_t *dog;
+    dog = malloc(sizeof(dog_t));
+    if (dog == NULL)
+    {
+        return (NULL);
+    }
+    dog->age = age;
+    dog->name = scopy(name);
+    if (!dog->name)
+    {
+        free(dog);
+        return (NULL);
+    }
+
+    dog->owner = scopy(owner);
+    if (!dog->owner)
+    {
+        free(dog->owner);
+        free(dog);
+        
+        return (NULL);
+    }
+}
