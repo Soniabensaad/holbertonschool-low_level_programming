d9239ea274fe176a6c5975abe5b79467fa9fbc06
Soniabensaad <5687@holbertonstudents.com>
2022-11-03 20:45:21 +0100

    print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,52 +2,48 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
- * print_strings - prints strings.
- * @separator: pointer.
- * @n: the number of integers passed to the function
- *
+ * print_all -  a function that prints anything.
+ * @format:typedef structure
  * Return: nothing
  */
 void print_all(const char * const format, ...)
 {
 va_list list;
-	int n = 0, i = 0;
-	char *separator = ", ";
-	char *s;
-
-	va_start(list, format);
-
-	while (format && format[i])
-    {
-		i++;
-    }
-	while (format && format[n])
-	{
-		if (n  == (i - 1))
-		{
-			separator = "";
-		}
-		switch (format[n])
-		{
-		case 'c':
-			printf("%c%s", va_arg(list, int), separator);
-			break;
-		case 'i':
-			printf("%d%s", va_arg(list, int), separator);
-			break;
-		case 'f':
-			printf("%f%s", va_arg(list, double), separator);
-			break;
-		case 's':
-			s = va_arg(list, char *);
-			if (s == NULL)
-				s = "(nil)";
-			printf("%s%s", s, separator);
-			break;
-		}
-		n++;
-	}
-	printf("\n");
-	va_end(list);
+int n = 0, i = 0;
+char *separator = ", ";
+char *s;
+va_start(list, format);
+while (format && format[i])
+{
+i++;
+}
+while (format && format[n])
+{
+if (n  == (i - 1))
+{
+separator = "";
+}
+switch (format[n])
+{
+case 'c':
+printf("%c%s", va_arg(list, int), separator);
+break;
+case 'i':
+printf("%d%s", va_arg(list, int), separator);
+break;
+case 'f':
+printf("%f%s", va_arg(list, double), separator);
+break;
+case 's':
+s = va_arg(list, char *);
+if (s == NULL)
+s = "(nil)";
+printf("%s%s", s, separator);
+break;
+}
+n++;
+}
+printf("\n");
+va_end(list);
 }
 
