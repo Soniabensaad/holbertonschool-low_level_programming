d3b5c46f0707d8e27f40c1431baf9e2b1b3e9ad7
Soniabensaad <5687@holbertonstudents.com>
2022-11-03 16:56:04 +0100

    print nmbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
- * sum_them_all - returns the sum of all its parameters.
- * @n: arguments.
+ * print_numbers - returns the sum of all its parameters.
+ * @separator: pointer.
+ * @n: the number of integers passed to the function
  *
- * Return: sum
+ * Return: nothing
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list ptr;
-    unsigned int i;
-    va_start(ptr, n);
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", va_arg(ptr, int));
-        if (separator != NULL && i < n-1)
-        {
-            printf("%s", separator);
-        }
-
-    }
-    printf("\n");
-    va_end (ptr);
-
+va_list ptr;
+unsigned int i;
+va_start(ptr, n);
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(ptr, int));
+if (separator != NULL && i < n - 1)
+{
+printf("%s", separator);
 }
+}
+printf("\n");
+va_end(ptr);
+}
+
