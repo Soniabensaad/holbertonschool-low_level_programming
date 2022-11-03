ac4f8ddaeffc63a27e593298d3acfdfbf305b164
Soniabensaad <5687@holbertonstudents.com>
2022-11-03 20:40:42 +0100

    print all

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -10,26 +10,26 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-unsigned int i;
-char *s;
-va_list ptr;
-va_start(ptr, n);
-for (i = 0; i < n; i++)
-{
-s = va_arg(ptr, char*);
-if (s)
-{
-printf("%s", s);
-}
-else
-{
-printf("(nil)");
-}
-if (separator != NULL && i < n - 1)
-{
-printf("%s", separator);
-}
-}
-printf("\n");
-va_end(ptr);
+    unsigned int i;
+    char *s;
+    va_list ptr;
+    va_start(ptr, n);
+    for (i = 0; i < n; i++)
+    {
+        s = va_arg(ptr, char *);
+        if (s)
+        {
+            printf("%s", s);
+        }
+        else
+        {
+            printf("(nil)");
+        }
+        if (separator != NULL && i < n - 1)
+        {
+            printf("%s", separator);
+        }
+    }
+    printf("\n");
+    va_end(ptr);
 }
---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - prints strings.
+ * @separator: pointer.
+ * @n: the number of integers passed to the function
+ *
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+va_list list;
+	int n = 0, i = 0;
+	char *separator = ", ";
+	char *s;
+
+	va_start(list, format);
+
+	while (format && format[i])
+    {
+		i++;
+    }
+	while (format && format[n])
+	{
+		if (n  == (i - 1))
+		{
+			separator = "";
+		}
+		switch (format[n])
+		{
+		case 'c':
+			printf("%c%s", va_arg(list, int), separator);
+			break;
+		case 'i':
+			printf("%d%s", va_arg(list, int), separator);
+			break;
+		case 'f':
+			printf("%f%s", va_arg(list, double), separator);
+			break;
+		case 's':
+			s = va_arg(list, char *);
+			if (s == NULL)
+				s = "(nil)";
+			printf("%s%s", s, separator);
+			break;
+		}
+		n++;
+	}
+	printf("\n");
+	va_end(list);
+}
+
