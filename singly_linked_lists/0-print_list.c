00f9c469f6e4ede1d18027d76684ddb2c45044a0
Soniabensaad <5687@holbertonstudents.com>
2022-11-14 22:27:18 +0100

    print list linked list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,14 +9,10 @@ size_t print_list(const list_t *h)
 size_t c = 0;
 while (h)
 {
-if (h->str)
-{
-printf("[%u]%s\n", h->len, h->str);
-}
-else
-{
+if (h->str == NULL)
 printf("[0] (nil)\n");
-}
+else
+printf("[%d] %s\n", h->len, h->str);
 h = h->next;
 c++;
 }
