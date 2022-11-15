ce6f669822ef47e1dcbdacb3e3b86f531e401e11
Soniabensaad <5687@holbertonstudents.com>
2022-11-15 19:31:39 +0100

    printbefore main

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,13 +1,12 @@
 #include "lists.h"
-void example (void) __attribute__ ((constructor));
+void example(void) __attribute__ ((constructor));
 /**
  * example - prints a sting before the main function
  * will be executated
  */
 void example(void)
 {
-    char *p;
-     p = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
-     printf("%s", p);
-    
+char *p;
+p = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+printf("%s", p);
 }
