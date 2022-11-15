1ce83e381dceb017b34fa38a37eed891732528c5
Soniabensaad <5687@holbertonstudents.com>
2022-11-15 16:15:10 +0100

    add node at begining

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,34 +1,33 @@
 #include "lists.h"
 /**
- * list_len - prints number of nodes
- * @h: pointer to a list
- * Return:number of nodes
+ * add_node - add a node at the begining of list_t
+ * @head: pointer to a list_t
+ * @str: pointer of char 
+ * Return:NULL or adress of new node
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-    char *duplicated;
-    int len = 0;
-    list_t *p;
-
-    p = malloc(sizeof(list_t));
-    
-    if (p == NULL)
-    {
-        return (NULL);
-    }
-    if (str == NULL)
-    {
-        return (NULL);
-    }
-    while (str[len] != '\0')
-    {
-        len++;
-    }
-    duplicated = strdup(str);
-    p->len = len;
-    p->str = duplicated;
-    p->next = *head;
-    *head = p;
-    return (p);
+char *duplicated;
+int len = 0;
+list_t *p;
+p = malloc(sizeof(list_t));    
+if (p == NULL)
+{
+return (NULL);
+}
+if (str == NULL)
+{
+return (NULL);
+}
+while (str[len] != '\0')
+{
+len++;
+}
+duplicated = strdup(str);
+p->len = len;
+p->str = duplicated;
+p->next = *head;
+*head = p;
+return (p);
 }
