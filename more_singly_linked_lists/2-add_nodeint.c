3a25cb8fc02691ba4c4e067f5131d7f5aa5e869e
Soniabensaad <5687@holbertonstudents.com>
2022-11-17 19:55:06 +0100

    insert node

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -9,14 +9,14 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 
-listint_t *p;
-p = malloc(sizeof(listint_t));
-if (p == NULL)
-{
-return (NULL);
-}
-p->n = n;
-p->next = *head;
-*head = p;
-return (p);
+    listint_t *p;
+    p = malloc(sizeof(listint_t));
+    if (p == NULL)
+    {
+        return (NULL);
+    }
+    p->n = n;
+    p->next = *head;
+    *head = p;
+    return (p);
 }
--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,27 +9,25 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 
-listint_t *p, *tp;
+    listint_t *p, *tp;
 
-tp = malloc(sizeof(listint_t));
-if (tp == NULL)
-{
-return (NULL);
+    tp = malloc(sizeof(listint_t));
+    if (tp == NULL)
+    {
+        return (NULL);
+    }
+    tp->n = n;
+    tp->next = NULL;
+    if (*head)
+    {
+        p = *head;
+        while (p->next != NULL)
+        {
+            p = p->next;
+        }
+        p->next = tp;
+    }
+    else
+        *head = tp;
+    return (tp);
 }
-tp->n = n;
-tp->next = NULL;
-if (*head)
-{
-p = *head;
-while (p->next != NULL)
-{
-p = p->next;
-}
-p->next = tp;
-}
-else
-*head = tp;
-return (tp);
-}
-
-
