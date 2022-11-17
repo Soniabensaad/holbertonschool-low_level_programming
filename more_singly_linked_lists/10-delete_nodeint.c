9af78f374a88c39a79e22925a9c25cb7647b7a14
Soniabensaad <5687@holbertonstudents.com>
2022-11-17 21:44:47 +0100

    delete node

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - deletes the node at index index
+ * @head: pointer to a listint_t
+ * @index :index
+ * Return:1 or -1
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+listint_t *tmp = *head;
+listint_t *node;
+unsigned int i = 1;
+if (!head || *head == NULL)
+{
+return (-1);
+}
+if (index == 0)
+{
+*head = tmp->next;
+free(tmp);
+tmp = NULL;
+return (1);
+}
+if (index == 1)
+{
+tmp = (*head)->next;
+(*head)->next = tmp->next;
+free(tmp);
+tmp = NULL;
+return (1);
+}
+while (i < index - 1)
+{
+tmp = tmp->next;
+i++;
+}
+node = tmp->next;
+tmp->next = node->next;
+free(node);
+node = NULL;
+return (1);
+}
--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -8,28 +8,26 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *ptr = *head;
-listint_t *ptr2;
-if (head == NULL)
-{
-return (NULL);
+    listint_t *ptr = *head;
+    listint_t *ptr2;
+    if (head == NULL)
+    {
+        return (NULL);
+    }
+    ptr2 = malloc(sizeof(listint_t));
+    if (ptr2 == NULL)
+    {
+        return (NULL);
+    }
+    ptr2->n = n;
+    ptr2->next = NULL;
+    idx--;
+    while (idx != 1)
+    {
+        ptr = ptr->next;
+        idx--;
+    }
+    ptr2->next = ptr->next;
+    ptr->next = ptr2;
+    return (NULL);
 }
-ptr2 = malloc(sizeof(listint_t));
-if (ptr2 == NULL)
-{
-return (NULL);
-}
-ptr2->n = n;
-ptr2->next = NULL;
-idx--;
-while (idx != 1)
-{
-ptr = ptr->next;
-idx--;
-}
-ptr2->next = ptr->next;
-ptr->next = ptr2;
-return (NULL);
-}
-
-
