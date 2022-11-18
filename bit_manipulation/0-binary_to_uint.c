010e654d13070fa8718a8b7acfb9c53881010e69
Soniabensaad <5687@holbertonstudents.com>
2022-11-18 13:06:44 +0100

    binary to unsigned int

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: const char
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+int j;
+unsigned int result = 0;
+unsigned int r = 1;
+unsigned int length;
+if (!b)
+{
+return (0);
+}
+length = strlen(b);
+for (j = length - 1; j >= 0; j--)
+{
+if (b[j] != '0' && b[j] != '1')
+{
+return (0);
+}
+if (b[j] == '1')
+{
+result = result + r;
+}
+r = r * 2;
+}
+return (result);
+}
---
+++ bit_manipulation/README.md
@@ -0,0 +1,2 @@
+C - Bit manipulation
+in c programming 
