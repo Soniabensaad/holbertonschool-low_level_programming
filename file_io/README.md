7138bf76f1abf2c535ddbd352e6766aecd886dac
Soniabensaad <5687@holbertonstudents.com>
2022-11-22 12:48:03 +0100

    readtext

---
+++ file_io/README.md
@@ -0,0 +1,2 @@
+C - File I/O
+in c programming 
---
+++ file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef FILE_IO
+#define FILE_IO
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif 
