52f6d31e30f80966d7bd86402141d68d86d325bf
Soniabensaad <5687@holbertonstudents.com>
2022-11-22 12:51:49 +0100

    readtext

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,37 +3,27 @@
 /**
  * read_textfile - reads a text file and prints it to the POSIX
  *@filename: a file text
- *@letters: size of char 
+ *@letters: size of char
  * Return: number of letters it could read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int i, j;
-    char *buffer ;
-    buffer = malloc(sizeof(char* )*letters);
-    if (buffer == NULL)
-    {
-        return (0);
-    }
-    i = open(filename, O_RDONLY);
-    if (i == -1)
-    {
-        return (0);
-    }
-    j = read(i, buffer, letters);
-    write(STDOUT_FILENO, buffer, j);
-    free(buffer);
-    close (i);
-    return (j);
-
-
-    
-
-
-
-    
-        
-   
-    
+int i, j;
+char *buffer;
+buffer = malloc(sizeof(char *) * letters);
+if (buffer == NULL)
+{
+return (0);
+}
+i = open(filename, O_RDONLY);
+if (i == -1)
+{
+return (0);
+}
+j = read(i, buffer, letters);
+write(STDOUT_FILENO, buffer, j);
+free(buffer);
+close(i);
+return (j);
 }
 
