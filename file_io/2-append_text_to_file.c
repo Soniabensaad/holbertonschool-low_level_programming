a93490fdcfe3b92d8c3481a5ec84f5289ef9f1c2
Soniabensaad <5687@holbertonstudents.com>
2022-11-22 17:25:19 +0100

    createfile

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * create_file -  creates a file.
+ *@text_content: content of char
+ *@filename:name of file content
+ * Return: 1 if success or -1 if it fails
+ */
