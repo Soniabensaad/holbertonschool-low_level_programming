284fd9c19b7ed8ed2ba487946db3e4ae186ec1f1
Soniabensaad <5687@holbertonstudents.com>
2022-11-22 17:13:19 +0100

    createfile

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,23 +8,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int fp, j, a;
-fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-if (fp == -1)
-{
-return (-1);
-}
-if (text_content == NULL)
-{
-text_content = '\0';
-}
-while (text_content[a])
-a++;
-j = write(fp, text_content, a);
-if (j == -1)
-{
-return (-1);
-}
-close(fp);
-return (1);
+    int fp, j, a;
+    fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+    if (fp == -1)
+    {
+        return (-1);
+    }
+    if (text_content == NULL)
+    {
+        text_content = "";
+    }
+    while (text_content[a])
+        a++;
+    j = write(fp, text_content, a);
+    if (j == -1)
+    {
+        return (-1);
+    }
+    close(fp);
+    return (1);
 }
