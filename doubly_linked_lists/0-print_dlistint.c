0780431ba9be96da327742178068c9c88596b3a9
Soniabensaad <5687@holbertonstudents.com>
2022-11-23 11:45:50 +0100

    number

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,7 @@
 /**
  * print_dlistint - prints all the elements of a dlistint_t list.
  *@h: a node pointer to head
- * Return: number of node
+ * Return: all elements of list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * dlistint_len-  returns the number of elements in a linked dlistint_t list
+ *@h: a node pointer to head
+ * Return: number of element of alist
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+int count = 0;
+while (h)
+{
+h = h->next;
+count++;
+}
+return (count);
+}
