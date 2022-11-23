38ea67f543befec20b754e96fc2af6e7854297e7
Soniabensaad <5687@holbertonstudents.com>
2022-11-23 10:11:53 +0100

    create file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
     }
     if (text_content)
     {
-        while (text_content[i] == '\0')
+        while (text_content[i] != '\0')
         {
             i++;
         }
