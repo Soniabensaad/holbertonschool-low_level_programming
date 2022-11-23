e83df6118873ec7aad72ed9de477d6ed2928ee1e
Soniabensaad <5687@holbertonstudents.com>
2022-11-23 16:30:38 +0100

    h=getnthnode

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,23 +1,23 @@
 #include "lists.h"
 /**
- * free_dlistint-  function that frees a dlistint_t list
+ * get_dnodeint_at_index-  returns the nth node of a dlistint_t linked list
  * @head: pointer to first node
- * Return: free a list
+ * @index: index of the node
+ * Return: nth node
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int a = 0;
-    dlistint_t *tmp;
-    tmp = head;
-    while (a < index)
-    {
-        if (!tmp)
-        {
-            return (NULL);
-        }
-        tmp = tmp->next;
-        a++;
-        
-    }
-    return (tmp);
+unsigned int a = 0;
+dlistint_t *tmp;
+tmp = head;
+while (a < index)
+{
+if (!tmp)
+{
+return (NULL);
+}
+tmp = tmp->next;
+a++;
+}
+return (tmp);
 }
