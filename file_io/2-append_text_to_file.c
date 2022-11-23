a51ec8a004eb293c808ca926702c239bd900eb01
Soniabensaad <5687@holbertonstudents.com>
2022-11-23 10:02:57 +0100

    create file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,8 +1,35 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * create_file -  creates a file.
+ * append_text_to_file -  appends text at the end of a file
  *@text_content: content of char
  *@filename:name of file content
  * Return: 1 if success or -1 if it fails
  */
+int append_text_to_file(const char *filename, char *text_content)
+{
+    int ptr, i = 0, j;
+    if (filename == NULL)
+    {
+        return (-1);
+    }
+    ptr = open(filename, O_WRONLY | O_APPEND);
+    if (ptr == -1)
+    {
+        return (-1);
+    }
+    if (text_content)
+    {
+        while (text_content[i] == '\0')
+        {
+            i++;
+        }
+        j = write(ptr, text_content, i);
+        if (j == -1)
+        {
+            return (-1);
+        }
+    }
+    close(ptr);
+    return (1);
+}
