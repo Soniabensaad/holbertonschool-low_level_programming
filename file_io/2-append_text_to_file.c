ea822779b1255f500841f43b606f51bd6c62e21e
Soniabensaad <5687@holbertonstudents.com>
2022-11-23 10:21:22 +0100

    create file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -8,28 +8,28 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int ptr, i = 0, j;
-    if (filename == NULL)
-    {
-        return (-1);
-    }
-    ptr = open(filename, O_WRONLY | O_APPEND);
-    if (ptr == -1)
-    {
-        return (-1);
-    }
-    if (text_content)
-    {
-        while (text_content[i] != '\0')
-        {
-            i++;
-        }
-        j = write(ptr, text_content, i);
-        if (j == -1)
-        {
-            return (-1);
-        }
-    }
-    close(ptr);
-    return (1);
+int ptr, i = 0, j;
+if (filename == NULL)
+{
+return (-1);
+}
+ptr = open(filename, O_WRONLY | O_APPEND);
+if (ptr == -1)
+{
+return (-1);
+}
+if (text_content)
+{
+while (text_content[i] != '\0')
+{
+i++;
+}
+j = write(ptr, text_content, i);
+if (j == -1)
+{
+return (-1);
+}
+}
+close(ptr);
+return (1);
 }
