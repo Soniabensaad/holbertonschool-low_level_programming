9222969dc0c820b42c7d418ed8c9bd249c0aabe5
Soniabensaad <5687@holbertonstudents.com>
2022-11-24 11:40:48 +0100

    insertnode

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,51 +1,41 @@
 #include "lists.h"
 /**
- * sum_dlistint-  returns the sum of datalist.
- * @head: pointer to first node
- * Return: sum of data
+ * insert_dnodeint_at_index-  inserts a new node at a given position
+ * @h: pointer to first node
+ * @idx:index
+ * @n:data
+ * Return: address of a new node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 
-    dlistint_t*new;
-    dlistint_t *temp = *h;
-    unsigned int a = 0;
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-    {
-        return (NULL);
-    }
-    temp->n = n;
-    temp = *h;
-    if (idx == 0)
-    {
-        new->next = *h;
-        (*h)->prev = new;
-        *h = new;
-    }
-    while (a < (idx -1))
-    {
-        temp = temp->next;
-        a++;
-    }
-    if(temp)
-    {
-        if(temp->next)
-        {
-            temp->next->prev = new;
-        }
-        new->prev = temp;
-        new->next = temp->next;
-        temp->next = new;
-       
-        
-    }
-    
-    return(new);
-    
-
-
-    
-    
-
+dlistint_t *new;
+dlistint_t *temp = *h;
+unsigned int a = 0;
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+{
+return (NULL);
+}
+new->n = n;
+temp = *h;
+if (idx == 0)
+{
+new->next = *h;
+(*h)->prev = new;
+*h = new;
+}
+while (a < (idx - 1))
+{
+temp = temp->next;
+a++;
+}
+if (temp)
+{
+temp->next->prev = new;
+new->next = temp->next;
+new->prev = temp;
+temp->next = new;
+}
+return (new);
 }
