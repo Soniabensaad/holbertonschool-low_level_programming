97f1495ee69ed231abcdf8271f6f007114f0e1e8
Soniabensaad <5687@holbertonstudents.com>
2022-11-25 19:11:30 +0100

    print hash

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -8,35 +8,33 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-    hash_node_t *a;
-    unsigned long int i, j = 0;
-    if (ht == NULL)
-    {
-        return;
-    }
-    printf("{");
-    for (i = 0; ht && i < ht->size; i++)
-    {
-        a = ht->array[i];
-        if (a != NULL)
-        {
-            if (j == 1)
-            {
-                printf(", ");
-            }
-        }
-            
-        while (a)
-            {
-                j = 1;
-                printf("'%s': '%s'", a->key, a->value);
-                if (a->next)
-                {
-                    printf(", ");
-                }
-                a = a->next;
-            }
-        
-    }
-    printf("}\n");
+hash_node_t *a;
+unsigned long int i, j = 0;
+if (ht == NULL)
+{
+return;
+}
+printf("{");
+for (i = 0; ht && i < ht->size; i++)
+{
+a = ht->array[i];
+if (a != NULL)
+{
+if (j == 1)
+{
+printf(", ");
+}
+}
+while (a)
+{
+j = 1;
+printf("'%s': '%s'", a->key, a->value);
+if (a->next)
+{
+printf(", ");
+}
+a = a->next;
+}     
+}
+printf("}\n");
 }
