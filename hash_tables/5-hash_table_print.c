8b4bcbebeebdd1656df09eee08b15ec45c07d949
Soniabensaad <5687@holbertonstudents.com>
2022-11-25 18:57:39 +0100

    print hash

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,14 +1,45 @@
 #include "hash_tables.h"
 #include <string.h>
 /**
- * hash_table_get- retrieves a value associated with a key.
- * @key: data
+ * hash_table_print- prints a hash table.
  * @ht: hashtable
- * Return: 1 or 0
+ * Return: value and key
  */
 
 void hash_table_print(const hash_table_t *ht)
 {
-    hash_table_t *table = hash_table_t *ht;
-    
+hash_node_t *a;
+unsigned long int i, j = 0;
+if (ht == NULL)
+{
+return;
+}
+printf("{");
+for (i = 0; ht && i < ht->size; i++)
+{
+a = ht->array[i];
+if (a != NULL && (j = 1))
+{
+printf(", ");
+}
+if (a)
+{
+if (!a->next)
+{
+printf("'%s': '%s'", a->key, a->value);
+j = 1;
+}
+while (a)
+{
+j = 1;
+printf("'%s': '%s'", a->key, a->value);
+if (a->next)
+{
+printf(", ");
+}
+a = a->next;
+}
+}
+}
+printf("}\n");
 }
