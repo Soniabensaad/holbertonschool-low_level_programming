a65b063b5901f4a5f533c1b15395e53d53a94492
Soniabensaad <5687@holbertonstudents.com>
2023-01-02 14:34:52 +0100

    linear search

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -6,29 +6,32 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-   size_t i, a = 0;
+   size_t i, j;
 
-	if (array == NULL)
-	{
-		return (-1);
-	}
-	for (i = 0; i <= size; i++)
-	{
-		if (array[i] != value && a == size)
-		{
-			return (-1);
-		}
-		else if (array[i] != value)
-		{
-			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-			a++;
-		}
-		else
-		{
-			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-			break;
-		}
-	}
-	return (i);
+    if (array == NULL)
+    {
+        return(-1);
+    }
+    
+    for (i = 0; i <= size; i++)
+    {
+        if (j == size && array[i] != value)
+        {
+            return -1;
+        }
+        else if (array[i] != value)
+        {
+            
+            printf("value  checked array[%ld] = [%d]\n", i, array[i]);
+            j++;
+        }
+        else
+        {
+            printf("value  checked array[%ld] = [%d]\n", i, array[i]);
+            break;
+        }
+    }
+    return(i);
+    
     
 }
