8773f9a6d007292c2932494e313f885615802c86
Soniabensaad <5687@holbertonstudents.com>
2023-01-03 10:37:58 +0100

    binary search

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,55 +1,54 @@
 #include "search_algos.h"
 /**
- * linear_search -  searches for a value in an array
- *@array: array of integer
- *@size: size of array
- *@value: value of array
- * Return: value
- */
-void print_arr(int *array, int s, int e)
+* print_arr- prints an array
+* @array: Array to search
+* @start: pointer to the start of array
+* @end: pointer to the end ofarray
+* Return: array in specific way
+*/
+void print_arr(int *array, int start, int end)
 {
-
-    printf("Searching in array : ");
-    for (; s <= e; s++)
-    {
-        printf("%d", array[s]);
-        if (s != e)
-        {
-            printf(", ");
-        }
-    }
-    printf("\n");
+	printf("Searching in array: ");
+	for (; start <= end; start++)
+	{
+		printf("%d", array[start]);
+		if (start != end)
+			printf(", ");
+	}
+	printf("\n");
 }
+
+
+/**
+* binary_search - bineary search algorithm
+* @array: Array to search
+* @size: Size of array
+* @value: Value to search for, -1 if not found
+* Return: Index where value
+*/
+
 int binary_search(int *array, size_t size, int value)
 {
-    int l ;
-    int h ;
-    int m;
-    if (array == NULL)
-    {
-        return (-1);
-    }
-    l = 0;
-    h = (int)size - 1;
-    while (l <= h)
-    {
-        m= (l + h) / 2;
-        print_arr(array, l, h);
-    
-        if (array[m] < value)
-        {
-            l = m + 1 ;
-        }
-        else if (array[m] > value)
-        {
-            h = m - 1;
-        }
-        else
-        {
-           return(m);
-        }
-    }
-    return (-1);
+	int low, high, middle;
 
+	if (array == NULL)
+		return (-1);
 
+	low = 0;
+	high = size - 1;
+	while (low <= high)
+	{
+		middle = (low + high) / 2;
+		print_arr(array, low, high);
+		if (array[middle] < value)
+			low = middle + 1;
+		else if (array[middle] > value)
+			high = middle - 1;
+		else
+		{
+			return (middle);
+		}
+	}
+
+	return (-1);
 }
