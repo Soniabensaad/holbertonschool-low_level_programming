efab7470e42b6aa5ea7a17aa97d046be6b51dbdc
Soniabensaad <5687@holbertonstudents.com>
2023-01-03 10:04:31 +0100

    binary search

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -6,51 +6,49 @@
  *@value: value of array
  * Return: value
  */
-void pray(int *array, int l, int r)
+void print_arr(int *array, int start, int end)
 {
-	printf("Searching in array: ");
-	for (; l <= r; l++)
-	{
-		printf("%d", array[l]);
-		if (l != r)
-			printf(", ");
-	}
-	printf("\n");
+
+    printf("Searching in array : ");
+    for (; start <= end; start++)
+    {
+        printf("%d", array[start]);
+        if (start != end)
+        {
+            printf(", ");
+        }
+    }
+    printf("\n");
 }
-
-
-/**
-* binary_search - bineary search algorithm
-* @array: Array to search
-* @size: Size of array
-* @value: Value to search for, -1 if not found
-* Return: Index where value
-*/
-
 int binary_search(int *array, size_t size, int value)
 {
-	int l, r, m;
+    int low = 0;
+    int high = size - 1;
+    int middle;
+    if (array == NULL)
+    {
+        return (-1);
+    }
+    
+    while (low <= high)
+    {
+        middle = (low + high) / 2;
+        print_arr(array, low, high);
+    
+        if (array[middle] == value)
+        {
+            return (value);
+        }
+        else if (array[middle] > value)
+        {
+            high = middle - 1;
+        }
+        else
+        {
+            low = middle + 1 ;
+        }
+    }
+    return (-1);
 
-	if (array == NULL)
-		return (-1);
 
-	l = 0;
-	r = (int)size - 1;
-	while (l <= r)
-	{
-		m = (l + r) / 2;
-		pray(array, l, r);
-		/**printf("array[%d]: %d\n", m, array[m]);*/
-		if (array[m] < value)
-			l = m + 1;
-		else if (array[m] > value)
-			r = m - 1;
-		else
-		{
-			return (m);
-		}
-	}
-	/**pray(array, l, r);*/
-
-	return (-1);
 }
